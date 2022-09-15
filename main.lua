c3c3b9e2df6d32ef804ed8b6b7f9579b575213e5
Impulse <impulsivecoder7@gmail.com>
2022-09-14 20:44:18 -0500

    bruh

---
+++ main.lua
@@ -0,0 +1,16 @@
+local config = require("config")
+
+local discordia = require('discordia')
+local client = discordia.Client()
+
+client:on('ready', function()
+	print('Logged in as '.. client.user.username)
+end)
+
+client:on('messageCreate', function(message)
+	if message.content == '!ping' then
+		message.channel:send('Pong!')
+	end
+end)
+
+client:run("Bot " .. config.token)
---
+++ package.lua
@@ -0,0 +1,15 @@
+  return {
+    name = "aaron-rebooted",
+    version = "0.0.1",
+    description = "World's worst discord bot. \"Thanks, Impulse.\" -Aaron",
+    tags = { "lua", "lit", "luvit" },
+    license = "MIT",
+    author = { name = "Impulse", email = "impulsivecoder7@gmail.com" },
+    homepage = "https://github.com/aaron-rebooted",
+    dependencies = {},
+    files = {
+      "**.lua",
+      "!test*"
+    }
+  }
+  
